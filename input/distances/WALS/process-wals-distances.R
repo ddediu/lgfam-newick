d3d49b88d23be685369c0cb1e4a0e00730ea08e7
Dan Dediu <ddediu@gmail.com>
2024-08-06 13:33:03 +0200

    Update the WALS version of 6 August 2024

--- input/distances/WALS/process-wals-distances.R
+++ input/distances/WALS/process-wals-distances.R
@@ -2,7 +2,7 @@
 #
 # Compute distances between languages using the WALS database.
 #
-# Copyright (C) 2014  Dan Dediu
+# Copyright (C) 2014=2024  Dan Dediu
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,8 +20,29 @@
 #
 #################################################################################################################
 
+# Load and merge the wals data in the forma required by the old code:
+# iso_code	glottocode	Name	latitude	longitude	genus	family feature_columns_starting_with_X
+wals_languages <- read.table( "../../wals/languages.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE );
+wals_languages <- unique(wals_languages[, c("ID", "ISO639P3code", "Glottocode", "Name", "Latitude", "Longitude", "Genus", "Family")]); # keep only the ones needed
+names(wals_languages) <- c("wals_code", "iso_code", "glottocode", "Name", "latitude", "longitude", "genus", "family");
+wals_languages <- wals_languages[ !is.na(wals_languages$iso_code) & wals_languages$iso_code != "", ]; # keep only the ones with a ISO code
+wals_features <- read.table( "../../wals/parameters.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE );
+wals_languages <- cbind(wals_languages, matrix(NA, nrow=nrow(wals_languages), ncol=nrow(wals_features))); 
+names(wals_languages)[9:ncol(wals_languages)] <- paste0("X", wals_features$ID);
+wals_values <- read.table( "../../wals/values.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE );
+# slow but clear:
+for( i in 1:nrow(wals_languages) )
+{
+  s <- wals_values[ wals_values$Language_ID == wals_languages$wals_code[i], ];
+  if( nrow(s) > 0 ) wals_languages[i, paste0("X",s$Parameter_ID) ] <- s$Value;
+}
+
+
+# --- ORIGINAL CODE ---
+
 # Load the wals data:
-wals <- read.table( "../../wals/language.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE );
+wals <- wals_languages; # rename it for compatibility with the old code
+#wals <- read.table( "../../wals/language.csv", header=TRUE, sep=",", quote="\"", stringsAsFactors=FALSE );
 
 # Keep only the numeric values of the features (discard the explanations):
 feat.cols <- grep( "^X", names(wals) );
@@ -30,7 +51,7 @@ for( i in 1:nrow(wals) )
   #cat( "Processing language ", i, " out of ", nrow(wals), "...\n" );
   for( j in feat.cols )
   {
-    if( wals[i,j] == "" )
+    if( is.na(wals[i,j]) || wals[i,j] == "" )
     {
       # Mark it as proper NA:
       wals[i,j] <- NA;
@@ -116,24 +137,24 @@ if( FALSE ) # takes some CPU time to run!
   # With missing data:
   mantel( wals.gower.dm,     wals.manhattan.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.65, p=0.001  <--- gower and manhattan are very similar
   mantel( wals.gower.dm,     wals.euclidean.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.40, p=0.001
-  mantel( wals.manhattan.dm, dwals.euclidean.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.41, p=0.001
+  mantel( wals.manhattan.dm, wals.euclidean.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.41, p=0.001
   
   # With mode imputation:
   mantel( wals.gower.mode.dm,     wals.manhattan.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.95, p=0.001  <--- gower and manhattan are very similar
-  mantel( wals.gower.mode.dm,     wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.53, p=0.001
-  mantel( wals.manhattan.mode.dm, wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.55, p=0.001
+  mantel( wals.gower.mode.dm,     wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.52, p=0.001
+  mantel( wals.manhattan.mode.dm, wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.54, p=0.001
     
   # Between missing data and mode imputation:
   mantel( wals.gower.dm,     wals.gower.mode.dm,     permutations=999, na.rm=TRUE, parallel=6 ); # r=0.15, p=0.001
-  mantel( wals.euclidean.dm, wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.20, p=0.001
+  mantel( wals.euclidean.dm, wals.euclidean.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.19, p=0.001
   mantel( wals.manhattan.dm, wals.manhattan.mode.dm, permutations=999, na.rm=TRUE, parallel=6 ); # r=0.27, p=0.001
 }
 
 
 # Save these distances to file:
-save( wals.gower.dm,     file="./wals-gower-dm.RData", compress="xz" );
-save( wals.manhattan.dm, file="./wals-manhattan-dm.RData", compress="xz" );
-save( wals.euclidean.dm, file="./wals-euclidean-dm.RData", compress="xz" );
+save( wals.gower.dm,          file="./wals-gower-dm.RData", compress="xz" );
+save( wals.manhattan.dm,      file="./wals-manhattan-dm.RData", compress="xz" );
+save( wals.euclidean.dm,      file="./wals-euclidean-dm.RData", compress="xz" );
 save( wals.gower.mode.dm,     file="./wals-gower-mode-dm.RData", compress="xz" );
 save( wals.manhattan.mode.dm, file="./wals-manhattan-mode-dm.RData", compress="xz" );
 save( wals.euclidean.mode.dm, file="./wals-euclidean-mode-dm.RData", compress="xz" );
--- input/wals/README.txt
+++ input/wals/README.txt
@@ -8,7 +8,11 @@ http://creativecommons.org/licenses/by-nc-nd/2.0/de/deed.en
 It should be cited as
 
 Dryer, Matthew S. & Haspelmath, Martin (eds.) 2013.
-The World Atlas of Language Structures Online.
-Leipzig: Max Planck Institute for Evolutionary Anthropology.
-(Available online at http://wals.info, Accessed on 2014-07-03.)
+WALS Online (v2020.3) [Data set]. Zenodo.
+https://doi.org/10.5281/zenodo.7385533
+(Available online at https://wals.info, Accessed on 2024-08-05.) 
+
+We downloaded here the files codes.csv, languages.csv, parameters.csv and 
+values.csv from the current version (5 August 2024) of WALS from
+https://github.com/cldf-datasets/wals/tree/master/cldf
 
